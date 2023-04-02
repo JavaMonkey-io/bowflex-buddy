0387550fb9fbbdb33119c4efb8c91faeee675c68
Chris Caspanello <ccaspanello@gmail.com>
2023-04-01 20:20:54 -0400

    [WIP] Fixing GitHub table rendering issue

--- src/pages/workouts/WorkoutsPage.tsx
+++ src/pages/workouts/WorkoutsPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import parse from 'html-react-parser'
-import {Session, Workout, workouts} from "../../data/workouts";
+import {Session, Workout, Set, workouts} from "../../data/workouts";
 import {exercises} from "../../data/exercises";
 import {Accordion, AccordionTab} from "primereact/accordion";
 import {Button} from "primereact/button";
@@ -12,21 +12,23 @@ const WorkoutsPage = () => {
 
   const navigate = useNavigate();
 
-  const exerciseBodyPart = (exerciseId: number) => {
-    return exercises.find((d)=>d.id === exerciseId)?.category;
+  const exerciseBodyPart = (s: Set) => {
+    return exercises.find((e)=>e.id === s.exerciseId)?.category;
   }
 
-  const exerciseName = (exerciseId: number) => {
-    return exercises.find((d)=>d.id === exerciseId)?.title;
+  const exerciseName = (s: Set) => {
+    return exercises.find((e)=>e.id === s.exerciseId)?.title;
   }
 
   const session = (s: Session) => {
+    console.log("s", s);
     return (
       <>
         <p className="text-lg font-semibold">{s.name}</p>
         <DataTable value={s.sets} size="small">
-          <Column header="Category" body={(d)=>exerciseBodyPart(d.exerciseId)} />
-          <Column header="Exercise" body={(d)=>exerciseName(d.exerciseId)} />
+          <Column header="Exercise" field="exerciseId"  />
+          <Column header="Category" body={(s)=>exerciseBodyPart(s)} />
+          <Column header="Exercise" body={(s)=>exerciseName(s)} />
           <Column header="Sets" body={(d) => <>{d.setCountMin}-{d.setCountMax}</>} />
           <Column header="Reps" body={(d) => <>{d.repCountMin}-{d.repCountMax}</>} />
         </DataTable>
@@ -39,6 +41,7 @@ const WorkoutsPage = () => {
       // TODO Work on Circuit Training
       return <></>
     }else{
+      console.log("w", w);
       return (
         <AccordionTab header={w.name}>
           <>
